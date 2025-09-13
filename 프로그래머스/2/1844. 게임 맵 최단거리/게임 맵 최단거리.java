182b7fe7fc9762ebb3d74119096155f445b68b46
John Park <devjohnpark@gmail.com>
2025-09-13 23:14:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.03 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,33 @@
 import java.util.*;
 
 class Solution {
-    // 최단 경로 찾기
-    // 맵에서 값이 1인곳으로만 상하좌우 이동 (가중치 없음)
-    // 가중치 없는 최단 경로 -> BFS가 적합
-    // 시작점 (0, 0)
-    // 도착지 (maps.length, maps[0].length)
     public int solution(int[][] maps) {
-        // 2차원 배열의 모든 시작으로 탐색을 진행 
-        // 이미 탐색한 지점은 bfs 메서드에서 0으로 변경
         int n = maps.length; // 2차원 배열의 row 
         int m = maps[0].length; // 2차원 배열의 column
         
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
+        int[] dx = {1, -1, 0, 0}; // x가 움직을 좌표
+        int[] dy = {0, 0, 1, -1}; // y가 움직을 좌표
         
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1});
-        maps[0][0] = 0; // 방문
+        queue.add(new int[]{0, 0, 1}); // 시작 x좌표 y좌표, 시작 칸을 포함해서 거리 1로 시작
+        maps[0][0] = 0; // 방문 
         
         while (!queue.isEmpty()) {
             int[] cur = queue.remove();
             int x = cur[0], y = cur[1];
-            
             int cnt = cur[2];
             
-            if(x == n-1 && y == m-1){
-                return cnt; // 최단 거리 반환 (bfs: 가장 처음 return 되는게 최단 거리)
+            if(x == n - 1 && y == m - 1){
+                return cnt; // 최단 거리 반환 (bfs에서 가장 처음 도달하는 것이 최단 거리)
             }
             
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 // 1을 발견하면 인접한 정점이므로 큐에 추가
                 if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1) {
                     maps[nx][ny] = 0; // 방문 처리
-                    queue.add(new int[]{nx, ny, cnt+1});
+                    queue.add(new int[]{nx, ny, cnt + 1}); // 시작점으로부터 현재 정점까지의 거리 저장
                 }
             }
         }
