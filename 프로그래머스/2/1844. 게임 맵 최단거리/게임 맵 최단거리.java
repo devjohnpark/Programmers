b945221524f9ed6b2fa86fd4347f684c25da2147
John Park <devjohnpark@gmail.com>
2025-11-14 08:26:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.23 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,54 @@
 import java.util.*;
 
+// 문제: 인접 행렬이 주어짐 -> 인접한 노드는 1로 그렇지 않은 0으로 그래프 표현
+// 문제 정의: (1,1)에서 (n,m)까지의 최단거리
+// 사용할 알고리즘: 가중치가 없으므로 BFS 사용
+// 핵심 로직
+// 상하좌우로 탐색
+// 방문한 곳은 0으로 변경
+// 탐색 지점의 값이 0/인덱스 x가 0보다 작거나 maps.length와 같거나 큼/인덱스 y가 0보다 작거나 maps[0].length와 같거나 크면 탐색 종료 -> -1 반환
+// 종료 조건 통과하면 거리 계산 변수에 +1 추가
+// 인덱스 x와 인덱스 y가 maps.length -1과 maps[1].length에 도달하면 최단 거리 변수에 기존 값보다 작으면 갱신
+
+// BFS 로직
+// 탐색한 노드를 큐에 적재
+// 큐를 빼내서 인접 큐를 모두 적재
+
 class Solution {
     public int solution(int[][] maps) {
-        int n = maps.length; // 2차원 배열의 row 
-        int m = maps[0].length; // 2차원 배열의 column
+        int n = maps.length;
+        int m = maps[0].length;
         
-        int[] dx = {1, -1, 0, 0}; // x가 움직을 좌표
-        int[] dy = {0, 0, 1, -1}; // y가 움직을 좌표
+        int[] dx = { 1, -1, 0, 0}; // x의 움직임 좌표
+        int[] dy = { 0, 0, 1, -1}; // y의 움직임 좌표
         
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1}); // 시작 x좌표 y좌표, 시작 칸을 포함해서 거리 1로 시작
-        maps[0][0] = 0; // 방문 
+        // x, y, 거리의 합 저장
+        Queue<int[]> q = new LinkedList<>();
         
-        while (!queue.isEmpty()) {
-            int[] cur = queue.remove();
-            int x = cur[0], y = cur[1];
-            int cnt = cur[2];
+        q.add(new int[] { 0, 0, 1});
+        
+        maps[0][0] = 0; // 방문 마킹
+        
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int d = cur[2];
             
-            if(x == n - 1 && y == m - 1){
-                return cnt; // 최단 거리 반환 (bfs에서 가장 처음 도달하는 것이 최단 거리)
-            }
+            // 도착지 도착
+            if (x == maps.length-1 && y == maps[0].length-1) return d;
             
+            // 상하좌우 탐색
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                // 1을 발견하면 인접한 정점이므로 큐에 추가
                 if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1) {
                     maps[nx][ny] = 0; // 방문 처리
-                    queue.add(new int[]{nx, ny, cnt + 1}); // 시작점으로부터 현재 정점까지의 거리 저장
+                    q.add(new int[]{nx, ny, d + 1}); // 시작점으로부터 현재 정점까지의 거리 저장
                 }
             }
-        }
-        return -1; // 도달 불가
+            
+        }    
+        return -1;
     }
 }
