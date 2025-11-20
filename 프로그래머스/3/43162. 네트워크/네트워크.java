ffb84bdeb8a871be66eb4198add4cf6a69a40741
John Park <devjohnpark@gmail.com>
2025-11-20 11:09:41 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    public int solution(int n, int[][] computers) {
+        int cnt = 0;
+        boolean[] visited = new boolean[computers.length];
+        for (int i = 0; i < computers.length; i++) {
+            if (!visited[i]) {
+                dfs(computers, i, visited);
+                cnt++;
+            }
+        } 
+        return cnt;
+    }
+    
+    private void dfs(int[][] computers, int node, boolean[] visited) {
+        visited[node] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[node][i] == 1 && !visited[i]) {
+                dfs(computers, i, visited);
+            }
+        }
+    }
+}
