e2eab49380a74614981f9a5d475674679ec31f18
John Park <devjohnpark@gmail.com>
2025-12-18 10:14:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 23316 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -13,40 +13,30 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        // 현재 거리를 x번 정점으로 두면, 시작 정점 5부터 x - 1, x + 1, x * 2 정점으로 확장시키면 그래프 형태가 나온다.
-        // 초당 특정 거리만큼 움직이므로 무가중치 그래프이다.
-        // 따라서 BFS 탐색해서 가장 먼저 도착 정점 17에 도달했을때 깊이를 반환하면 시간이다.
+        Queue<int[]> q = new LinkedList<>();
+        boolean[] visited = new boolean[100001];
 
-        // int 거리를 저장할 큐
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[100001]; // 정점 N의 방문 마킹을 위한 변수
-
-        q.add(N);
+        q.add(new int[] { N, 0 });
         visited[N] = true;
 
-        int time = 0;
-
         while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int curPosition = q.remove(); // 방문 정점
+            int[] cur = q.remove();
+            int curPosition = cur[0];
+            int curDist = cur[1];
 
-                if (curPosition == K) {
-                    System.out.println(time);
-                    return;
-                }
+            if (curPosition == K) {
+                System.out.println(curDist);
+                return;
+            }
 
-                // x - 1, x + 1, x * 2 정점 생성
-                int[] nextPositions = { curPosition - 1, curPosition + 1, curPosition * 2 };
+            int[] nextPositions = { curPosition - 1, curPosition + 1, curPosition * 2 };
 
-                for (int nextPosition: nextPositions) {
-                    if (nextPosition >= 0 && nextPosition <= 100000 && !visited[nextPosition]) {
-                        visited[nextPosition] = true;
-                        q.add(nextPosition);
-                    }
+            for (int nextPosition: nextPositions) {
+                if (nextPosition >= 0 && nextPosition <= 100000 && !visited[nextPosition]) {
+                    visited[nextPosition] = true;
+                    q.add(new int[] {nextPosition, curDist + 1});
                 }
             }
-            time++;
         }
     }
 }
