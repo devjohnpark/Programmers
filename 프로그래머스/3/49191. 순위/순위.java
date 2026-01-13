b99e2c1aae35bb98e2a1492eb0318bccb6af47fc
John Park <devjohnpark@gmail.com>
2026-01-14 08:50:14 +0900

    [level 3] Title: 순위, Time: 9.26 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,10 +1,8 @@
-// 순위를 매길려면 
 import java.util.*;
 
 class Solution {
-    
     public int solution(int n, int[][] results) {
-        // win/lose 그래프 생성
+        // win/lose 그래프를 인접 리스트로 생성
         List<Integer>[] winGraph = new ArrayList[n+1];
         List<Integer>[] loseGraph = new ArrayList[n+1];
         
@@ -14,46 +12,43 @@ class Solution {
             loseGraph[i] = new ArrayList<>();
         }
         
-        for (int i = 0; i < results.length; i++) {
-            int win = results[i][0];
-            int lose = results[i][1];
-            winGraph[win].add(lose);
-            loseGraph[lose].add(win);
+        // 주어진 배열로부터 win/lose 그래프 값 저장
+        for (int[] result: results) {
+            int a = result[0];
+            int b = result[1];
+            winGraph[a].add(b);
+            loseGraph[b].add(a);
         }
         
-        // win graph와 lose graph가 동시에 같은 정점을 시작으로 dfs 순회
-        // dfs 순회로 반환한 인접 정점의 개수가 n -1이면 카운팅 
-        int rankCnt = 0;
+        // 순위가 매겨진 정점 카운팅
+        int rankFixCnt = 0;
+        
+        // 각 정점을 시작점으로부터 탐색
+        // 탐색 시작전 방문 배열 초기화
         for (int i = 1; i <= n; i++) {
             boolean[] visited = new boolean[n+1];
-            int winCnt = dfs(i, winGraph, visited);
+            int winCnt = dfs(winGraph, i, visited);
             
             visited = new boolean[n+1];
-            int loseCnt = dfs(i, loseGraph, visited);
-            if (winCnt + loseCnt == n - 1) rankCnt++;
+            int loseCnt = dfs(loseGraph, i, visited);
+            
+            // 자기 자신을 제외한 정점의 총 정점의 개수 - 1과 동일하면 순위 매겨짐
+            if (winCnt + loseCnt == n - 1) rankFixCnt++;
         }
-        return rankCnt;
+        
+        return rankFixCnt;
+          
     }
     
-    // 이미 순회를 마친 정점은 개수를 카운트?
-    
-    // 2 -> 1 -> 3 -> 4
-    // 5 -> 2
-    
-
-    private int dfs(int start, List<Integer>[] graph, boolean[] visited) {
-        // 재귀적으로 다음 정점을 순회
-        visited[start] = true;
-        int cnt = 0; // 자기 자신을 제외해야하므로 0
-        // 해당 정점의 인접 정점으로 순회
-        for (int adjV: graph[start]) {
-            // 탐색한 인접 정점 카운트
-            if (!visited[adjV]) {
-                cnt++;
-                cnt += dfs(adjV, graph, visited); // dfs가 호출될때마다 카운트
+    private int dfs(List<Integer>[] graph, int start, boolean[] visited) {
+        int count = 0;
+        visited[start] = true; 
+        for (int next : graph[start]) {
+            if (!visited[next]) {
+                count++; 
+                count += dfs(graph, next, visited);
             }
         }
-        return cnt;
+        return count;
     }
-   
 }
