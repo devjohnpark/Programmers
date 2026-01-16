4a61577b1cbc452c1bab33ba975e2089095e1eb7
John Park <devjohnpark@gmail.com>
2026-01-16 09:35:36 +0900

    [level 1] Title: 모의고사, Time: 2.90 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -6,39 +6,30 @@ class Solution {
         int[] nums2 = { 2, 1, 2, 3, 2, 4, 2, 5};
         int[] nums3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int nums1Cnt = 0;
-        int nums2Cnt = 0;
-        int nums3Cnt = 0;
+        int[] scores = new int[3];
         
         for (int i = 0; i < answers.length; i++) { 
             if (answers[i] == nums1[i % nums1.length]) {
-                nums1Cnt++;        
+                scores[0]++;        
             }
             
             if (answers[i] == nums2[i % nums2.length]) {
-                nums2Cnt++;        
+                scores[1]++;        
             }
             
             if (answers[i] == nums3[i % nums3.length]) {
-                nums3Cnt++;        
+                scores[2]++;        
             }
         }
         
-        int[][] sortCnt = { {nums1Cnt, 1}, {nums2Cnt, 2}, {nums3Cnt, 3} };
+        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
         
-        Arrays.sort(sortCnt, (a, b) -> Integer.compare(b[0], a[0]));
-    
-        if (sortCnt[0][0] == 0) return new int[] {};
+        List<Integer> result = new ArrayList<>();
         
-        List<Integer> list = new ArrayList<>();
-        list.add(sortCnt[0][1]);
+        for (int i = 0; i < scores.length; i++) {
+            if (max == scores[i]) result.add(i + 1);
+        }
         
-        for (int i = 1; i < sortCnt.length; i++) {
-            if (sortCnt[0][0] == sortCnt[i][0]) {
-                list.add(sortCnt[i][1]);
-            }
-        } 
-        
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
