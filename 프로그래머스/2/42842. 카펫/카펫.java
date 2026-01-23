6581d36f7d14eb5947b22509ba5a2ad31aea8dc7
John Park <devjohnpark@gmail.com>
2026-01-23 09:41:06 +0900

    [level 2] Title: 카펫, Time: 26.36 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -8,11 +8,10 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         // Y로 생성할수있는 모든 직사각형의 경우의 수를 저장
         // Y와 B로 합쳐서 생성 가능한 가로 >= 세로인 직사각형을 저장
-        // 모든 카페트 경우의 숫에서 Y의 가로와 세로보다 +2이상인 직사각형만 반환 배열에 저장
+        // 모든 카페트 경우의 숫에서 Y의 가로와 세로보다 +2이상인 직사각형을 반환한다.
         
         List<int[]> yellows = new ArrayList<>();
         List<int[]> carpets = new ArrayList<>();
-        List<int[]> result = new ArrayList<>();
         
         for (int i = 1; i <= yellow; i++) {
             if (yellow % i == 0) {
@@ -41,18 +40,11 @@ class Solution {
                 int yw = y[0];
                 int yh = y[1];
                 if (cw >= yw + 2 && ch >= yh + 2) {
-                    result.add(new int[] { cw, ch });
+                    return new int[] { cw, ch };
                 }
             }
             
         }
-        
-        // carpets를 돈다.
-        // yellows를 돈다.
-        // yellows의 요소중 각 가로와 세로보다 2이상 큰 카페트를 result에 저장한다.
-        
-        
-        return result.get(0);
-        
+        return new int[] {};
     }
 }
