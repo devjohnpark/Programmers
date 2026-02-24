df0cae06ff689dc4f8753ce3dcfa2fd1b474ba90
John Park <devjohnpark@gmail.com>
2026-02-24 10:37:48 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 112 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -3,14 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// N: 50, M: 8
-// 사각형의 개쉬: (N - M + 1)^2
-// 사각형 내 모든 칸 탐색: M^2
-// M x M 사각형 내에서 첫번째 칸을 W/B 로 시작한 체스판 중, 최소 변경 값으로 저장
-// 부르트 포스 시간 복잡도: (N - M + 1)^2 x M^2 = 43^2 x 8^2 x 2 < 10^8 이라서 통과
-
-// 시작점으로부터 8개 이후면 탐색 x
-// 시작점으로 부터 상하좌우를 이동하면서 색이 이전과 다르지 않은 개수를 탐색
+// 사각형의 개쉬: (행 - 8 + 1) x (열 - 8 + 1)
+// 사각형 내 모든 칸 탐색: 64
+// 8 x 8 사각형 내에서 첫번째 칸을 W/B 로 시작한 체스판 중, 최소 변경 해야하는 값으로 갱신
+// 부르트 포스 시간 복잡도: (행 - 8 + 1) x (열 - 8 + 1) x 64 x 2 < 10^8 이라서 통과
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,6 +23,8 @@ public class Main {
 
         int min = 64;
         char c;
+        
+        // (n - 8 + 1) x (m - 8 + 1) x 2 x 8^2 = 43^2 x 2 x 8^2 
         // 가로/세로 8인 정사각형 내에서
         for (int rowSt = 0; rowSt <= n - 8; rowSt++) {
             for (int colSt = 0; colSt <= m - 8; colSt++) {
@@ -38,6 +36,7 @@ public class Main {
                         c = 'B';
                     }
                     int count = 0;
+                    // 8 x 8 정사각형 순회
                     for (int row = rowSt; row < rowSt + 8; row++) {
                         for (int col = colSt; col < colSt + 8; col++) {
                             if (board[row][col] != c) {
