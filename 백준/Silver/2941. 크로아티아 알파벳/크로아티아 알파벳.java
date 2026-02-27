960f5934754251b25827f841010f2f6927e612b7
John Park <devjohnpark@gmail.com>
2026-02-27 11:11:08 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -3,27 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String s = br.readLine();
-
+        String word = br.readLine();
         String[] targets = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
         int cnt = 0;
-        for (int i = 0; i < s.length(); i++) {
+        // 0, 1,
+        for (int i = 0; i < word.length(); i++) {
             for (int j = 0; j < targets.length; j++) {
-                if (s.startsWith(targets[j], i)) {
+                if (word.startsWith(targets[j], i)) {
                     i += targets[j].length() - 1;
-                    break;
                 }
             }
             cnt++;
         }
         System.out.println(cnt);
-
     }
-
 }
-
