f2a96edc9f27b33c004c7502149e74efe7ad9aad
John Park <devjohnpark@gmail.com>
2026-02-27 22:21:00 +0900

    [level 2] Title: 더 맵게, Time: 1541.29 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -15,8 +15,6 @@ class Solution {
     // 최소힙
     // 1. 배열 요소 모두 삽입
     
-    // 횟수 카운팅 -1로 초기화
-    
     // 반복
     // 1. 최소힙의 최솟값이 k보다 같거나 크면 or 빈 최소힙이 아니면 반복문 탈출
     // 2. 최소힙의 저장된 값의 개수가 2보다 작으면 -1 반환
@@ -28,25 +26,34 @@ class Solution {
     // 횟수 카운팅 변수 반환
 
     
+    // 정렬 nlogn = 10^6 x 6log10^6
     public int solution(int[] scoville, int K) {
         int answer = 0;
+
+		// 1. `PriorityQueue`을 최소힙으로 생성한다.
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // 디폴트 오름차순
-        // 내림차순
-        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        // O(nlogn)
+
+		// 2. 배열을 순회하면서 우선순위큐에 넣는다.
         for (int n: scoville) {
             minHeap.add(n);
         }
+
+		// 3. 우선순위큐가 비지않거나 가장 작은 값이 `K`보다 작으면 반복한다.
+        while (!minHeap.isEmpty() && minHeap.peek() < K) {
+	        // 4. 우선순위큐의 크기가 2보다 작으면, K 이상으로 만들 수 없으므로 -1을 반환한다.
+            if (minHeap.size() < 2) return -1; 
+
+			// 5. 우선순위큐에서 가장 작은 2개의 값을 차례로 삭제해서 공식에 대입한 새로운 값으로 만든다.
+            int min1 = minHeap.remove(); // 가장 작은 값
+            int min2 = minHeap.remove(); // 두번째로 가장 작은 값
+
+			// 6. 새로운값을 다시 우선순위큐에 넣는다.
+            minHeap.add(min1 + (min2 * 2)); 
         
-         while (!minHeap.isEmpty() && minHeap.peek() < K) {
-            if (minHeap.size() < 2) return -1; // 더 섞을 재료가 부족하면 불가능
-
-            int min1 = minHeap.remove(); // 최소
-            int min2 = minHeap.remove(); // 두 번째 최소
-
-            minHeap.add(min1 + (min2 * 2));
-            answer++;
+	        // 7. 공식을 대입한 회수를 카운팅한다.
+            answer++; 
         }
+        // 8. 카운팅한 회수를 반환한다.
         return answer;
     }
 }
