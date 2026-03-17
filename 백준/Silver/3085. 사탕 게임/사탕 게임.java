620039bd0347e3e7a7b154ec3406b81af5f8e367
John Park <devjohnpark@gmail.com>
2026-03-17 09:10:12 +0900

    [Silver II] Title: 사탕 게임, Time: 336 ms, Memory: 79320 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 1. 서로 다른 색의 인접칸 요소 바꿔치기 (롤백 필요: 매개 변수 복사 배열)
+// 2. 모든 행/열 중에서 가장 긴 것 찾아서 개수 반환
+
+public class Main {
+    static int[] dx = { 1, -1, 0 , 0};
+    static int[] dy = { 0, 0, 1 , -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[][] map = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+        int max = 0;
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < N; y++) {
+                for (int k = 0; k < 4; k++) {
+                    char[][] tmpMap = copyMap(map);
+                    int nx = x + dx[k];
+                    int ny = y + dy[k];
+                    if (nx >= 0 && ny >= 0 && nx < N && ny < N && tmpMap[x][y] != tmpMap[nx][ny]) {
+                        // 인접한 두 칸 바꿔치기
+                        char color = tmpMap[x][y];
+                        tmpMap[x][y] = tmpMap[nx][ny];
+                        tmpMap[nx][ny] = color;
+
+                        int cnt;
+
+                        // 모든 같은 색의 행 중에서 가장 큰 긴값
+                        for (int i = 0; i < N; i++) {
+                            cnt = 1;
+                            for (int j = 1; j < N; j++) {
+                                if (tmpMap[i][j] == tmpMap[i][j - 1]) {
+                                    cnt++;
+                                } else {
+                                    cnt = 1;
+                                }
+                                max = Math.max(max, cnt);
+                            }
+                        }
+
+                        // 모든 같은 색의 열 중에서 가장 큰 긴값
+                        for (int j = 0; j < N; j++) {
+                            cnt = 1;
+                            for (int i = 1; i < N; i++) {
+                                if (tmpMap[i][j] == tmpMap[i - 1][j]) {
+                                    cnt++;
+                                } else {
+                                    cnt = 1;
+                                }
+                                max = Math.max(max, cnt);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static char[][] copyMap (char[][] map) {
+        char[][] newMap = new char[map.length][map[0].length];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                newMap[i][j] = map[i][j];
+            }
+        }
+        return newMap;
+    }
+}
+
+
+
